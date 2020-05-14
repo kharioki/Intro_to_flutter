c8c38b0d61053f7a0b4f4d781271681d606ed855
kharioki <antolisah@gmail.com>
2020-05-14 16:37:32 +0300

    using stateless widgets and hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Avengers'),
-          centerTitle: true,
-          backgroundColor: Colors.pinkAccent[400],
-        ),
-        body: Center(
-          child: Text(
-            'Hello avengers!',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              fontFamily: 'IndieFlower',
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-            ),
+void main() => runApp(
+      MaterialApp(
+        home: Home(),
+      ),
+    );
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Avengers'),
+        centerTitle: true,
+        backgroundColor: Colors.pinkAccent[400],
+      ),
+      body: Center(
+        child: Text(
+          'Hello avengers!!',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'IndieFlower',
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text('click'),
-          backgroundColor: Colors.pinkAccent[400],
-        ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.pinkAccent[400],
+      ),
+    );
+  }
+}
