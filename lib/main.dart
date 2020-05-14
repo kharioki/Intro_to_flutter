120da7778cf4434de7be2a43eb781e2b89fe756b
kharioki <antolisah@gmail.com>
2020-05-14 16:11:12 +0300

    applying colors and adding custom fonts

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,24 @@ void main() => runApp(MaterialApp(
         appBar: AppBar(
           title: Text('Avengers'),
           centerTitle: true,
+          backgroundColor: Colors.pinkAccent[400],
         ),
         body: Center(
-          child: Text('Hello avengers'),
+          child: Text(
+            'Hello avengers!',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'IndieFlower',
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+            ),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
           child: Text('click'),
+          backgroundColor: Colors.pinkAccent[400],
         ),
       ),
     ));
